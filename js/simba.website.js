d21189376e519c0cbd6b4030aa79b858b671dde8
unknown <>
2015-12-17 11:25:58 +0800

    新增轮播

--- js/simba.website.js
+++ js/simba.website.js
@@ -308,49 +308,144 @@ $.fn.slider = function(opts){
 	opts = $.extend({
 		leftBtn : "", //左按钮
 		rightBtn : "", //右按钮
+		imgW : 0,  //轮播图片的宽度
 		imgH : 0,  //轮播图片的高度
-		timeout : 4000,	//轮播间隔时间
-		btnShow : true, //是否显示左右按钮
-		addTag : true,	//是否创建圆点标记
+		timeout : 1000,	//轮播间隔时间
+		moveTime : 300, //动画时间
 		autoSlider : true, //是否自动轮播
+		moveStyle : "slide", //动画效果  slide ：滑动，fade ：渐隐渐现
+		addTag : true,	//是否创建圆点标记
+		tagSize : 12,
+		tagDefaultBg : "#fff", //圆点标记的默认背景
+		tagActiveBg : "#5638d8", // 圆点标记选中背景
+		btnShow : true, //是否显示左右按钮
+		btnW : 30,	//按钮的宽度
+		btnH : 60,	//按钮的高度
+		btnBg : "rgba(0,0,0,.6)", //左右按钮的背景
 	});
 
 	var ths = this,
 		$imgs = ths.find("img"),
 		$thsLink = ths.find("a"),
-		imgSize = $imgs.size();
+		imgSize = $imgs.size(),
+		$btn,$tag,
+		count = 0;
 
 	var init = function(){
 		initCss();
-		startSlider();
 		if(opts.addTag){
 			createTag();
 		}
+		if(opts.btnShow){
+			createBtn();
+		}
+		$thsLink.eq(0).css({"left" : 0});
+		$tag.find("span").eq(0).css({"background-color" : opts.tagActiveBg});
+		startSlider();
 	},
 	//初始化样式
 	initCss = function(){
 		ths.css({
+			"position" : "relative",
 			"height" : opts.imgH + "px",
-			"overflow" : "hidden"
+			"overflow" : "hidden",
+			"background-color" : "#fff",
+			"z-index" : "9"
 		});
 		$thsLink.css({
 			"display" : "block",
+			"position" : "absolute",
+			"top" : "0",
+			"left" : "100%",
 			"width" : "100%",
 			"height" : "100%",
+			"z-index" : "9"
 		});
 		$imgs.css({
 			"width" : "100%",
 			"height" : "100%",
 		});
 	},
+	//创建左右按钮
+	createBtn = function(){
+		var btnTem = '<span class="s_btn"></span><span class="s_btn"></span>';
+		ths.append(btnTem);
+		$btn = ths.find(".s_btn");
+		$btn.css({
+			"position" : "absolute",
+			"top" : "50%",
+			"margin-top" : (-1)*opts.btnH/2 + "px",
+			"width" : opts.btnW + "px",
+			"height" : opts.btnH+ "px",
+			"cursor" : "pointer",
+			"background-color" : opts.btnBg,
+			"z-index" : "10"
+		});
+		$btn.eq(0).css({"left" : "10px"});
+		$btn.eq(1).css({"right" : "10px"});
+	}
 	//创建圆点标记
 	createTag = function(){
-		var tem = '<div class=""></div>';
+		var tem = '<div class="s_tag"></div>';
 		ths.append(tem);
+		$tag = ths.find(".s_tag");
+		$tag.css({
+			"position" : "absolute",
+			"left" : "0",
+			"bottom" : "0",
+			"width" : "100%",
+			"height" : "40px",
+			"line-height" : "40px",
+			"font-size" : "0",
+			"text-align" : "center",
+			"z-index" : "10"
+		});
+		for(var i = 0; i < imgSize; i ++){
+			$tag.append("<span></span>");
+		}
+		$tag.find("span").css({
+			"display" : "inline-block",
+			"margin-top" : (40 - opts.tagSize)/2 + "px", 
+			"margin-left" : "5px",
+			"margin-right" : "5px",
+			"width" : opts.tagSize + "px",
+			"height" : opts.tagSize + "px",
+			"background-color" : opts.tagDefaultBg,
+			"border-radius" : "9999px",
+			"cursor" : "pointer"
+		});
 	},
 	//轮播动画
 	imgsMove = function(){
-
+		count ++;
+		count = count == imgSize ? 0 : count;
+		if(opts.moveStyle == "slide"){
+			$thsLink.eq(count).stop().animate({
+				"left": 0},
+				opts.moveTime, function() {
+				var countR = (count == imgSize - 1 ? -1 : count) + 1;
+				$thsLink.eq(countR).css({"left":"100%"});
+			});
+			$thsLink.eq(count-1).stop().animate({
+				"left": "-100%"},
+				opts.moveTime);
+		}else if(opts.moveStyle == "fade"){
+			$thsLink.css({"left": 0});
+			$thsLink.stop().fadeOut(opts.moveTime).eq(count).fadeIn(opts.moveTime);
+		}
+		tagsMove();
+		startSlider();
+	},
+	//圆点标记移动
+	tagsMove = function(){
+		if(opts.addTag){
+			var $tagsObj = $tag.find("span");
+			$tagsObj.css({
+				"background-color" : opts.tagDefaultBg
+			}).eq(count).css({
+				"background-color" : opts.tagActiveBg
+			});
+		}
 	},
 	//开始轮播
 	startSlider = function(){
